f0a0f4f1c466f711a941d02431e11cb99f5c0e7c
blaukc <brandonlau5501@gmail.com>
2018-11-05 16:16:13 +0800

    added TTS and qna mode

--- main.js
+++ main.js
@@ -5,9 +5,13 @@ var timer = true;
 var min = 0, sec = '00';
 var i = 0, j = 0, t;
 var ding = new Audio('ding.wav');
+var questionList, asked = [], n;
 
 window.onload = function () {
     document.getElementById('time').innerHTML = min + ':' + sec;
+    jQuery.get('questions.txt', function (data) {
+        questionList = data.split('\n');
+    });
 };
 
 function increaseTime() {
@@ -33,11 +37,39 @@ function increaseTime() {
     document.getElementById('time').innerHTML = min + ':' + sec;
 }
 
-function start() {
+function startTime() {
     ding.play();
-    document.getElementById('start').style.visibility = 'hidden';
-    document.getElementById('mode').style.visibility = 'hidden';
     t = setInterval(increaseTime, 1000);
+}
+
+function readQuestion() {
+    while (true) {
+        n = Math.floor(Math.random() * questionList.length);
+        if (!asked.includes(n)) {
+            asked.push(n);
+            break;
+        }
+        if (asked.length === questionList.length) {
+            asked = [];
+        }
+    }
+    window.utterances = [];
+    var qn = new SpeechSynthesisUtterance(questionList[n]);
+    utterances.push(qn);
+    speechSynthesis.speak(qn);
+    qn.onend = function () {
+        startTime();
+    };
+}
+
+function start() {
+    document.getElementById('start').style.visibility = 'hidden';
+    document.getElementById('modeToggle').style.visibility = 'hidden';
+    if (mode === 'speech') {
+        startTime();
+    } else if (mode === 'qna') {
+        readQuestion();
+    }
 }
 
 function sidebar() {
@@ -47,6 +79,7 @@ function sidebar() {
         document.getElementsByClassName('toggle')[0].style.opacity = '1';
         document.getElementsByClassName('toggle')[1].style.opacity = '1';
         document.getElementsByClassName('toggle')[2].style.opacity = '1';
+        document.getElementsByClassName('toggle')[3].style.opacity = '1';
         x = false;
     } else {
         document.getElementById('sidebar').style.width = '0px';
@@ -54,27 +87,28 @@ function sidebar() {
         document.getElementsByClassName('toggle')[0].style.opacity = '0';
         document.getElementsByClassName('toggle')[1].style.opacity = '0';
         document.getElementsByClassName('toggle')[2].style.opacity = '0';
+        document.getElementsByClassName('toggle')[3].style.opacity = '0';
         x = true;
     }
 }
 
 function changeMode() {
     if (mode === 'speech') {
-        document.getElementById('mode').innerHTML = 'Q&A';
+        document.getElementById('modeToggle').innerHTML = 'Q&A';
         mode = 'qna';
     } else if (mode === 'qna') {
-        document.getElementById('mode').innerHTML = 'Speech';
+        document.getElementById('modeToggle').innerHTML = 'Speech';
         mode = 'speech';
     }
 }
 
 function changeTimer() {
     if (timer) {
-        document.getElementById('timer').innerHTML = 'Hide Timer';
+        document.getElementById('timerToggle').innerHTML = 'Hide Timer';
         document.getElementById('time').style.opacity = '1';
         timer = false;
     } else {
-        document.getElementById('timer').innerHTML = 'Show Timer';
+        document.getElementById('timerToggle').innerHTML = 'Show Timer';
         document.getElementById('time').style.opacity = '0';
         timer = true;
     }
@@ -82,8 +116,8 @@ function changeTimer() {
 
 function reset() {
     clearInterval(t);
-    i = 0
-    j = 0
+    i = 0;
+    j = 0;
     document.getElementById('reset').style.visibility = 'hidden';
     document.getElementById('start').style.visibility = 'visible';
     document.getElementById('mode').style.visibility = 'visible';
